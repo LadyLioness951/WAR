1478067a6b9c6405b47a5102541ac7181ce51dcb
Megan Hays <ladylioness@megans-imac.lan>
2021-06-06 10:54:54 -0700

    Add Master Deck and Shuffled Deck to HTML. Cleaned out CSS and adding btn hover, border-radius, and card-holder. Getting rid of player turn since both players go at the same time adding visible hidden button styles add gameStatus of null. Add event listeners.

--- js/main.js
+++ js/main.js
@@ -1,49 +1,84 @@
 /*----- constants -----*/
 const suits = ["d", "s", "h", "c"];
-const rants = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"];
-
+const ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"];
+const howToWin;
 const masterDeck = buildMasterDeck();
-renderDeckInContainer(masterDeck, document.getElementById("master-deck-container"));
+
 /*----- app's state (variables) -----*/
 let shuffledDeck;
+let gameStatus;
+let cards;
+let playerHand;
+let computerHand;
 
 /*----- cached element references -----*/
-const shuffledContainer = document.getElementById("shuffled-deck-container");
+const msgEl = document.getElementById("msg");
+const buttonEl = document.getElementById("replay");
+const buttonEls = document.getElementById("start");
 
 /*----- event listeners -----*/
-document.querySelector("button").addEventListener(click, renderNewShuffledDeck);
+document.getElementById("replay").addEventListener(click, renderNewShuffledDeck);
+document.getElementById("start").addEventListener(click, renderNewShuffledDeck);
+document.getElementById("board").addEventListener(click, renderCardClick);
 
 /*----- functions -----*/
 function init() {
+    gameStatus = null;
     const player1Deck = newDeck.splice(0, 26);
     const player2Deck = newDeck;
 
     render();
 }
 
-function renderNewShuffledDeck() {
-    shuffledDeck = getNewShuffledDeck();
-    renderDeckInContainer(shuffledDeck, shuffledContainer);
+function render() {
+    buttonEl.style.visibility = gameStatus ? "hidden" : "visible";
+    buttonEls.style.visibility = gameStatus ? "visible" : "hidden";
+
+    renderMessage();
+    renderCardClick();
 }
 
-function renderDeckInContainer(deck, container) {
-    container.innerHTML = "";
-    let cardsHtml = "";
-    deck.forEach(function(card) {
-        cardsHtml += `div class="card ${card.face}"></div>`;
-    });
-    container.innerHTML = cardsHtml;
+function renderCardClick(){
+
+}
+
+function renderMessage() {
+    if(gameStatus === "lose") {
+        msgEl.innerText = "You Lose!";
+    } else if(gameStatus) {
+        msgEl.innerText = `${players[gameStatus].name} WINNER!`;
+    } else {
+        msgEl.innerText = "Good Luck!";
+    }
+}
+
+function getNewShuffledDeck() {
+    const tempDeck = [...masterDeck];
+    const newShuffledDeck = [];
+    while (tempDeck.length) {
+      const rndIdx = Math.floor(Math.random() * tempDeck.length);
+      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+    }
+    return newShuffledDeck;
+  }
+
+function renderNewShuffledDeck() {
+    shuffledDeck = getNewShuffledDeck();
 }
 
 function buildMasterDeck() {
     const deck = [];
+    // Use nested forEach to generate card objects
     suits.forEach(function(suit) {
-        ranks.forEach(function(rank) {
-            deck.push({
-                face: `${suit}#{rank}`,
-                value: 
-            });
+      ranks.forEach(function(rank) {
+        deck.push({
+          // The 'face' property maps to the library's CSS classes for cards
+          face: `${suit}${rank}`,
+          value: (rank % 13)+1
         });
     });
-    return deck;
+  });
+  return deck;
 }
+
+renderNewShuffledDeck();
